b19387d50002d43057fa8bda4f4b863beed86550
Kim Jørgensen <kj@kjsoft.dk>
2019-10-07 23:21:25 +0200

    ChuckNorris

--- Startup.cs
+++ Startup.cs
@@ -29,6 +29,7 @@ namespace test
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<ChuckNorrisService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
